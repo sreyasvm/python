a1d8c7fd56d63ffc64231c0f66fef0f46b0aebd0
Sreyas <vmsreyas@gmail.com>
2021-05-28 17:36:00 +0200

    added test and insert api

--- django/mysite/polls/tests.py
+++ django/mysite/polls/tests.py
@@ -1,4 +1,19 @@
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
+from .models import Question
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_date(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        future_date = timezone.now() + datetime.timedelta(days=20)
+        future_question = Question(pub_date = future_date)
+        self.assertIs(future_question.was_published_recently(),False)
 
 
 
--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,15 +1,22 @@
+import random
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from django.http import JsonResponse
+from django.utils import timezone
 
 # Create your views here.
 
 def index(request):
     latest_questions_list = Question.objects.order_by('pub_date')[:5]
     response = []
+    # send question text as response
     for q in latest_questions_list:
-        response.append(q.question_text)
+        key_value = {}
+        key_value['question'] = q.question_text
+        key_value['date'] = q.pub_date
+        response.append(key_value)
     return JsonResponse(response, safe=False)
 
 def detail(request,question_id):
@@ -21,5 +28,11 @@ def results(request,question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def insert(request):
+    text = "Bazinga " + str(random.randrange(100)) + " ?"
+    question = Question(question_text = text, pub_date=timezone.now())
+    question.save()
+    return JsonResponse(question.question_text, safe=False)
 
 
